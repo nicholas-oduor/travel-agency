8716e3eac8b2add8f28e707b89c6866ac2219561
seron-ux <abyseronkirui@gmail.com>
2021-01-14 12:42:36 +0300

    changes made

--- contact.js
+++ contact.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 $("button").click(function(){
 var name = document.getElementById('name').value;
 
-    alert("Thanks" + ""+ name +''+ "for being our client ,your message has been received");
+    alert("Thanks " + ""+ name +''+ "for being our client ,your message has been received");
     });
     });
