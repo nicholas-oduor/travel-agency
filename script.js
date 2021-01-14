91ac86f51195328b2bc057995ff02570692607c8
nicholas-oduor <oduor5742@gmail.com>
2021-01-14 12:42:21 +0300

    made several changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    function contact(form) {
-        let name = document.form["contacts"]["name"].value;
-        let email = document.form["contacts"]["email"].value;
-        let message = document.form["contacts"]["message"].value;
-        alert("Hello! " + name + "," + " We have received your message. Thank you for reaching out to us. ");
-    }
-}
+$("#contact").submit(function (event) {
+    event.preventDefault();
+    var name1 = $("#form1").val();
+    var email1 = $("#form2").val();
+    var message1 = $("#form3").val();
+    alert("Hello! " + name1 + "," + " We have received your message. Thank you for reaching out to us. ");
+
+});
